851581e3a7514ffd5fde06a97bcfc48ec50786f5
Yehor Bondarchuk <C1Y_cdFdJi>
2021-05-11 13:14:11 +0300

    Fix problems

--- lab3/src/main/com/nure/MainServlet.java
+++ lab3/src/main/com/nure/MainServlet.java
@@ -13,7 +13,7 @@ public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("message", "");
+        req.setAttribute("text", "");
         req.getRequestDispatcher("index.jsp").forward(req, resp);
     }
 
